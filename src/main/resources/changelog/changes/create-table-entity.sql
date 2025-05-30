61d8c887661cbbf31d940212c90f1baaaf34e82a
sanja_r_q <ersainsanjar02.06@gmail.com>
2025-05-30 17:49:46 +0500

    Create liquibase create table

---
+++ src/main/resources/changelog/changes/create-table-entity.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--changeset sanzhar:1
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    name VARCHAR(255),
+    password VARCHAR(255),
+    telegram_token varchar(100) unique not null ,
+    chat_id varchar(100) not null
+
+
+);
+
+
+
+
