6c2b78cbaeeb878675e29cc12474de5929f22727
sanja_r_q <ersainsanjar02.06@gmail.com>
2025-05-30 18:03:33 +0500

    Create RestController.java

---
+++ src/main/java/com/example/berkutsanzharbot/controller/RestController.java
@@ -0,0 +1,12 @@
+package com.example.berkutsanzharbot.controller;
+
+
+import lombok.RequiredArgsConstructor;
+
+@org.springframework.web.bind.annotation.RestController
+@RequiredArgsConstructor
+public class RestController {
+
+
+
+}
